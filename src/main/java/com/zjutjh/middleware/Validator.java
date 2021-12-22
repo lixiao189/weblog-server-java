0823b594144a312c97c6d217aa8a72cc68459bfe
Node Sans <node189@icloud.com>
2021-12-22 15:12:18 +0800

    完善用户检验中间件

--- src/main/java/com/zjutjh/middleware/Validator.java
+++ src/main/java/com/zjutjh/middleware/Validator.java
@@ -1,13 +1,30 @@
 package com.zjutjh.middleware;
 
+import com.zjutjh.Helper;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.Session;
 
 public class Validator {
     public static void checkAuth(RoutingContext context) {
+        Session session = context.session();
+        if (session.get("username") != null) {
+            context.next();
+        }
 
+        context.json(new JsonObject(Helper.notLoginResponse()));
     }
 
     public static void checkPrivilege(RoutingContext context) {
+        Session session = context.session();
+        if (session.get("username") != null) {
+            if (Integer.parseInt(session.get("administrator")) == 1) {
+                context.next();
+            } else {
+                context.json(new JsonObject(Helper.notAdministratorResponse()));
+            }
+        }
 
+        context.json(new JsonObject(Helper.notLoginResponse()));
     }
 }
