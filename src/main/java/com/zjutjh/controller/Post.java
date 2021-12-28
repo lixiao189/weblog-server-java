5687f6d20c556943cdf75260a05a14cf99469f15
Node Sans <node189@icloud.com>
2021-12-28 23:30:22 +0800

    修改了删除帖子接口，使其没有回调地狱

--- src/main/java/com/zjutjh/controller/Post.java
+++ src/main/java/com/zjutjh/controller/Post.java
@@ -2,6 +2,7 @@ package com.zjutjh.controller;
 
 import com.zjutjh.App;
 import com.zjutjh.Helper;
+import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.Session;
@@ -59,30 +60,27 @@ public class Post {
         boolean isAdministrator = (int) session.get("administrator") == 1;
 
         String queryPostStmt = "select  * from posts where id = ?";
-        App.getMySQLClient().preparedQuery(queryPostStmt).execute(Tuple.of(postID), ar -> {
-            if (ar.result().size() == 0) {
+        App.getMySQLClient().preparedQuery(queryPostStmt).execute(Tuple.of(postID)).compose(ar -> {
+            if (ar.size() == 0) {
                 context.json(new JsonObject(Helper.respData(1, "数据不存在", null)));
-                return;
+                return Future.failedFuture("数据不存在");
             }
 
             int trueSenderID = -1;
-            for (Row row : ar.result()) {
+            for (Row row : ar) {
                 trueSenderID = row.getInteger("sender_id");
             }
 
             if (trueSenderID != senderID && !isAdministrator) {
                 context.json(new JsonObject(Helper.respData(2, "只能删除自己的帖子", null)));
-                return;
+                return Future.failedFuture("只能删除自己的帖子");
             }
 
             String deletePostStmt = "delete from posts where id = ?";
-            App.getMySQLClient().preparedQuery(deletePostStmt).execute(Tuple.of(postID), deleteAr -> {
-                if (deleteAr.succeeded()) {
-                    context.json(new JsonObject(Helper.respData(0, "删除成功", null)));
-                } else {
-                    System.out.println(deleteAr.cause().getMessage()); // 记录失败的原因
-                }
-            });
+            return App.getMySQLClient().preparedQuery(deletePostStmt).execute(Tuple.of(postID));
+        }).compose(ar -> {
+            context.json(new JsonObject(Helper.respData(0, "删除成功", null)));
+            return Future.succeededFuture();
         });
     }
 
